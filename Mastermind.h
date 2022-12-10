c5cf84d04a04afe80b5bbd9eec63de77ba00acfb
andrewy1n <67555036+andrewy1n@users.noreply.github.com>
2022-12-10 11:23:24 -0800

    Update Mastermind.h

--- Mastermind.h
+++ Mastermind.h
@@ -12,34 +12,39 @@
 #include <stack>
 #include <queue>
 #include <list>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-class Mastermind 
-{
+class Mastermind {
 private:
     bool allowDuplicates;
     int codeLength;
-    int turns;
-    set<string> colors = {"red", "green", "blue", "yellow", "brown", "orange", "black", "white"};;
-    map<int, string> answer;
+    bool win;
+    set<string> colors = {"red", "green", "blue", "yellow", "brown", "orange", "black", "white"};
     list<string> currentGuess;
     stack<pair<int, int>> guessResults;
     queue<list<string>> guesses;
+    vector<bool> bloomFilter;
 
-public:
-    Mastermind();  // Default constructor
-    ~Mastermind(); // Destructor
+    void setTurns(int);
     void setDuplicates(bool);
     void setCodeLength(int);
-    void setTurns(int);
-    map<int, string> genAnswer();  // Generate answer
     bool validGuess(list<string>); // Check for validity of guesses
     pair<int, int> checkGuess(list<string>);
     bool checkWin();
+    void outputVisuals();
+    unsigned int RSHash (const string&);
+
+public:
+    int turns;
+    map<int, string> answer;
+    map<int, string> genAnswer();  // Generate answer
+
+    Mastermind();  // Default constructor
+    ~Mastermind(); // Destructor
     void preGame();
     void gameLoop();
-    void outputVisuals();
 };
 
 Mastermind::Mastermind()
@@ -47,6 +52,9 @@ Mastermind::Mastermind()
     this->allowDuplicates = false;
     this->codeLength = 4;
     this->turns = 10;
+    this->win = false;
+    for(int i = 0; i < 100; i++){this->bloomFilter.push_back(0);}
+    for(auto itr = colors.begin(); itr != colors.end(); itr++){bloomFilter.at(RSHash(*itr)%100) = 1;}
 }
 
 Mastermind::~Mastermind()
@@ -59,6 +67,21 @@ Mastermind::~Mastermind()
 
     queue<list<string>> emptyQueue;
     swap(this->guesses, emptyQueue);
+}
+
+unsigned int Mastermind::RSHash(const string& str)
+{
+   unsigned int b    = 378551;
+   unsigned int a    = 63689;
+   unsigned int hash = 0;
+
+   for(size_t i = 0; i < str.length(); i++)
+   {
+      hash = hash * a + str[i];
+      a    = a * b;
+   }
+
+   return hash;
 }
 
 void Mastermind::setCodeLength(int length)
@@ -115,17 +138,9 @@ bool Mastermind::validGuess(list<string> guess)
         return false;
     }
 
-
     for (auto itr = guess.begin(); itr != guess.end(); ++itr)
     { // if the guess is the correct length, check if colors inputted are valid
-        if (colors.count(*itr) == 1)
-        {
-            continue; // the color is in colors array, so continue the loop
-        }
-        else
-        {
-            return false; // the color is not in the colors set, check is not valid
-        }
+        if(this->bloomFilter.at(RSHash(*itr)%100)==0) return false;
     }
     return true; // if we pass both of these checks, we have ourselves a valid guess from the player
 }
@@ -219,72 +234,67 @@ void Mastermind::preGame()
         cin >> turns;
     }
     setTurns(turns);
+    this->answer = genAnswer();
 }
 
 void Mastermind::gameLoop()
-{
-    this->answer = genAnswer();
-    bool win;
-    cin.ignore(10000, '\n');
-    for (int i = 0; i < this->turns; i++)
+{   
+    static int turnCounter = turns;
+
+    if(turnCounter == this->turns){
+        cin.ignore(10000, '\n');
+    }
+    this->currentGuess.clear();
+    while (!validGuess(this->currentGuess))
     {
-        currentGuess.clear();
-        while (!validGuess(this->currentGuess))
-        {
-            if (i != 0)
-            {
-                outputVisuals();
-            }
-            cout << endl
-                 << "Colors: Red, Green, Blue, Yellow, Brown, Orange, Black, White " << endl
-                 << "Guess: ";
-            string color;
-           
-            getline(cin, color);
-            string delimiter = " ";
-            size_t pos = 0;
-            string token;
-            while ((pos = color.find(delimiter)) != std::string::npos) {
-                token = color.substr(0, pos);
-                transform(token.begin(), token.end(), token.begin(), ::tolower);
-                this->currentGuess.push_back(token);
-                color.erase(0, pos + delimiter.length());
-            }
-            transform(color.begin(), color.end(), color.begin(), ::tolower);
-            this->currentGuess.push_back(color);
-
-            if (!validGuess(this->currentGuess))
-            {
-                currentGuess.clear();
-                cout << endl << "Invalid input. Try Again." << endl;
-            }
+        outputVisuals();
+        cout << endl
+                << "Colors: Red, Green, Blue, Yellow, Brown, Orange, Black, White " << endl
+                << "Guess: ";
+        string color;
+        
+        getline(cin, color);
+        string delimiter = " ";
+        size_t pos = 0;
+        string token;
+        while ((pos = color.find(delimiter)) != string::npos) {
+            token = color.substr(0, pos);
+            transform(token.begin(), token.end(), token.begin(), ::tolower);
+            this->currentGuess.push_back(token);
+            color.erase(0, pos + delimiter.length());
         }
+        transform(color.begin(), color.end(), color.begin(), ::tolower);
+        this->currentGuess.push_back(color);
 
-        guesses.push(currentGuess);
-        guessResults.push(checkGuess(this->currentGuess));
-
-        win = checkWin();
-        if (win)
+        if (!validGuess(this->currentGuess))
         {
-            break;
+            this->currentGuess.clear();
+            cout << endl << "Invalid input. Try Again." << endl;
         }
     }
 
-    outputVisuals();
-    cout << endl;
-    if (win) // if you win
-        cout << "You win!";
-    else // if you lost
-        cout << "You lost.";
-    cout << endl
-         << endl
-         << "Answer: "; // output answer
+    guesses.push(this->currentGuess);
+    guessResults.push(checkGuess(this->currentGuess));
 
-    for (auto it = answer.begin(); it != answer.end(); it++)
-    {
-        cout << it->second << " ";
+    this->win = checkWin();
+
+    if(turnCounter==0||this->win==true){
+        cout << endl;
+        if (win) // if you win
+            cout << "You win!";
+        else // if you lost
+            cout << "You lost.";
+        cout << endl << endl << "Answer: "; // output answer
+
+        for (auto it = answer.begin(); it != answer.end(); it++)
+        {
+            cout << it->second << " ";
+        }
+        cout << endl;
+    }else{
+        turnCounter -= 1;
+        gameLoop();
     }
-    cout << endl;
 }
 
 void Mastermind::outputVisuals()
@@ -292,9 +302,9 @@ void Mastermind::outputVisuals()
     queue<list<string>> copyGuesses = this->guesses;
     stack<pair<int, int>> copyResults = this->guessResults;
     cout << endl
-         << "Turns Remaining: " << turns - this->guesses.size() << endl
+         << "Turns Remaining: " << this->turns - guesses.size() << endl
          << endl
-         << setw(codeLength) << left << "Guesses" << setw(5);
+         << setw(codeLength) << "Guesses" << setw(5);
     cout << " | [Red : White]" << endl;
     while (!copyGuesses.empty())
     {
